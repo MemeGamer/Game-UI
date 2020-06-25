130dc8b0e0a812beebd5cb75f32ffed2842d8511
Varun VP <30493776+varunidealogy@users.noreply.github.com>
2020-06-25 22:56:52 +0530

    Add files via upload

---
+++ MainMenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+   public void PlayGame()
+   {
+       SceneManager.LoadScene("SampleScene");
+   }
+
+   public void EndGame()
+   {
+       Application.Quit();
+   }
+
+   public void Mute() {
+       {
+           AudioListener.pause = !AudioListener.pause;
+       }
+   }
+
+   public void GameRestart()
+   {
+      StartCoroutine(RestartGame());
+   }
+   IEnumerator RestartGame()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Mymenu()
+   {
+       SceneManager.LoadScene("Main menu");
+   }
+}
